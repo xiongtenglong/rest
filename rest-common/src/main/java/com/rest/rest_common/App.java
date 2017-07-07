28e1fe4c5605d5935a1cbc125ed87bd4fc6014c1
DongNanXing <1142048288@qq.com>
2017-07-07 14:29:18 +0800

    测试14：29

--- rest-common/src/main/java/com/rest/rest_common/App.java
+++ rest-common/src/main/java/com/rest/rest_common/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("23423");
         System.out.println("123456");
+        System.out.println("88888");
     }
 }
