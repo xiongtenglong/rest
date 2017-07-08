013ec35331306c20902e153103edaf125a892d46
xiongtenglong <876433478@qq.com>
2017-07-08 15:14:58 +0800

    配置前端跨域dobbox

--- rest-web/src/main/java/com/rest/web/controller/HouseAreaController.java
+++ rest-web/src/main/java/com/rest/web/controller/HouseAreaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.rest.dubbox.pojo.HouseArea;
 import com.rest.dubbox.service.DubboxHouseAreaService;
@@ -16,6 +17,7 @@ public class HouseAreaController {
 	@Autowired
 	private DubboxHouseAreaService dubboxHouseAreaService;
 	@RequestMapping("/allCity")
+	@ResponseBody
 	public List<HouseArea> queryAreaList(){
 		return (List<HouseArea>) dubboxHouseAreaService.queryallCity().getData();
 	}
