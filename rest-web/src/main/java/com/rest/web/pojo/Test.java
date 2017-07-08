9496b53974210d7089b16acbe7a37c00529ac1dd
xiongtenglong <876433478@qq.com>
2017-07-08 14:53:59 +0800

    删除连个测试类

--- rest-web/src/main/java/com/rest/web/pojo/Test.java
+++
@@ -1,9 +0,0 @@
-package com.rest.web.pojo;
-
-public class Test {
-
-    public void test(){
-        System.out.println("321432");
-    }
-
-}
--- rest-web/src/main/java/com/rest/web/service/Test.java
+++
@@ -1,8 +0,0 @@
-package com.rest.web.service;
-
-public class Test {
-	public static void main(){
-		System.out.println("sdfdsfds");
-		System.out.println("23432");
-	}
-}
