234302e150d980019c8b27eaf009e660e18ea4a3
xiongtenglong <876433478@qq.com>
2017-07-08 14:04:01 +0800

    jre

---
+++ rest-dubbox/src/main/java/com/rest/dubbox/service/DubboxHouseAreaService.java
@@ -0,0 +1,20 @@
+package com.rest.dubbox.service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
+import com.rest.common.vo.SysResult;
+
+@Path("houseArea")
+@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
+public interface DubboxHouseAreaService {
+
+	@Path("allCity")
+	@GET
+	public SysResult queryallCity();
+}
