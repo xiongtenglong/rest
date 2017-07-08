3585331a83b4c4a64e7cc17b0c01b2eabc169447
ronghuihu <893253744@qq.com>
2017-07-08 14:49:01 +0800

    房屋所在区域mapper

--- rest-web/src/main/java/com/rest/web/mapper/HouseAreaMapper.java
+++ rest-web/src/main/java/com/rest/web/mapper/HouseAreaMapper.java
@@ -1,10 +1,11 @@
 package com.rest.web.mapper;
 
 import com.rest.common.mapper.SysMapper;
+import com.rest.web.pojo.HouseArea;
 import com.rest.web.pojo.HouseInfo;
 
 /**
  * Created by Administrator on 2017/7/8.
  */
-public interface HouseAreaMapper extends SysMapper<HouseInfo> {
+public interface HouseAreaMapper extends SysMapper<HouseArea> {
 }
