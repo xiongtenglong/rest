d4e69f764a97186ee41656dcd65d31d637412add
xiongtenglong <876433478@qq.com>
2017-07-08 14:23:07 +0800

    删除干扰类

--- rest-manage/src/main/java/com/rest/manage/mapper/HouseAreaMapper.java
+++
@@ -1,8 +0,0 @@
-package com.rest.manage.mapper;
-
-import com.rest.common.mapper.SysMapper;
-import com.rest.dubbox.pojo.HouseArea;
-
-public interface HouseAreaMapper extends SysMapper<HouseArea>{
-
-}
