ebedf8a18efcfc2229bf38efd7cdbdfd0a949180
qingfeng1202 <844539509@qq.com>
2017-07-11 20:28:23 +0800

    用户信息中心

---
+++ rest-manage/src/main/java/com/rest/manage/service/MessageCenterService.java
@@ -0,0 +1,31 @@
+package com.rest.manage.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rest.manage.mapper.HouseInfoMapper;
+import com.rest.manage.mapper.UserMapper;
+import com.rest.manage.pojo.HouseInfo;
+import com.rest.manage.pojo.User;
+
+@Service
+public class MessageCenterService {
+	@Autowired
+	private UserMapper userMapper;
+	@Autowired
+	private HouseInfoMapper houseInfoMapper;
+	//根据ID查询用户信息
+	public User findUserById(Long id) {
+		User user = userMapper.selectByPrimaryKey(id);
+		return user;
+	}
+	public List<HouseInfo> findHouseById(Long id) {
+		HouseInfo house = new HouseInfo();
+		house.setUserId(id);
+		List<HouseInfo> houses = houseInfoMapper.select(house);
+		return houses;
+	}
+	
+}
