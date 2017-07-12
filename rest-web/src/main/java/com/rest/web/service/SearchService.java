26a931ba941f7ba3f7eae45911cc1ba4baa2af76
ronghuihu <893253744@qq.com>
2017-07-12 14:23:52 +0800

    solr

---
+++ rest-web/src/main/java/com/rest/web/service/SearchService.java
@@ -0,0 +1,40 @@
+package com.rest.web.service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.rest.web.pojo.HouseInfo;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class SearchService {
+	@Autowired
+	private HttpSolrClient httpSolrClient;
+
+	public List<HouseInfo> queryItemList(String q, Integer page, Integer rows) throws IOException, SolrServerException {
+		if (null == page){
+			page = 1;
+		}
+		Integer startPos = (Math.max(page, 1)-1) * rows;
+
+		SolrQuery query = new SolrQuery();
+		query.setQuery("title:" + q);
+		query.setStart(startPos);
+		query.setRows(rows);
+
+
+		QueryResponse response = httpSolrClient.query(query);
+		List<HouseInfo> itemList = response.getBeans(HouseInfo.class);
+		return itemList;
+	}
+
+}
