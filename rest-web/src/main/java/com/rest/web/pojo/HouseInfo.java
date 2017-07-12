60805904d06a370e33e871e868a55d8e576f87e6
ronghuihu <893253744@qq.com>
2017-07-12 14:08:04 +0800

    solr搜索

--- rest-web/src/main/java/com/rest/web/pojo/HouseInfo.java
+++ rest-web/src/main/java/com/rest/web/pojo/HouseInfo.java
@@ -1,18 +1,33 @@
 package com.rest.web.pojo;
 
 import com.rest.common.po.BasePojo;
+import org.apache.solr.client.solrj.beans.Field;
 
 
 public class HouseInfo extends BasePojo {
+	@Field
 	private Long houseId;//房屋Id
+	@Field
 	private Long userId;//用户Id
+	@Field
 	private String title;//标题
-	private Long prive;//价格，单位：分
+	@Field
+	private Long price;//价格，单位：分
+	@Field
 	private String houseDesc;//描述
-	private String imagePath;//图片地址
+	@Field
+	private String imageUrl;//图片地址
+	@Field
 	private String address;//地址
+	@Field
 	private String notice;//房屋公告
+	@Field
 	private Integer status;//房屋状态 ：1-正常，2-下架，3-删除
+	@Field
+	private Integer areaId;
+	@Field
+	private Integer evaluate;
+	@Field
 	private Integer statusAuth;//认证状态：1-未认证，2-认证中，3-已认证
 	public Long getHouseId() {
 		return houseId;
@@ -32,24 +47,30 @@ public class HouseInfo extends BasePojo {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public Long getPrive() {
-		return prive;
-	}
-	public void setPrive(Long prive) {
-		this.prive = prive;
-	}
+
 	public String getHouseDesc() {
 		return houseDesc;
 	}
 	public void setHouseDesc(String houseDesc) {
 		this.houseDesc = houseDesc;
 	}
-	public String getImagePath() {
-		return imagePath;
+
+	public Long getPrice() {
+		return price;
 	}
-	public void setImagePath(String imagePath) {
-		this.imagePath = imagePath;
+
+	public void setPrice(Long price) {
+		this.price = price;
 	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
 	public String getAddress() {
 		return address;
 	}
@@ -74,5 +95,19 @@ public class HouseInfo extends BasePojo {
 	public void setStatusAuth(Integer statusAuth) {
 		this.statusAuth = statusAuth;
 	}
-	
+	public Integer getEvaluate() {
+		return evaluate;
+	}
+
+	public void setEvaluate(Integer evaluate) {
+		this.evaluate = evaluate;
+	}
+
+	public Integer getAreaId() {
+		return areaId;
+	}
+
+	public void setAreaId(Integer areaId) {
+		this.areaId = areaId;
+	}
 }
