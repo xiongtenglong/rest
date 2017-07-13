7bc4dcca524892e3d63535e28e823fc03de7091c
xiongtenglong <876433478@qq.com>
2017-07-13 08:08:46 +0800

    更新user

--- rest-web/src/main/java/com/rest/web/pojo/User.java
+++ rest-web/src/main/java/com/rest/web/pojo/User.java
@@ -1,13 +1,11 @@
 package com.rest.web.pojo;
 
-import java.util.Date;
+import com.rest.common.po.BasePojo;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import com.rest.common.po.BasePojo;
 
 @Table(name="tb_user")
 public class User extends BasePojo{
@@ -18,6 +16,24 @@ public class User extends BasePojo{
 	private String username;
 	private String password;
 	private String phone;
+	private UserInfo userInfo;  //用户的扩展信息
+
+	private String name;
+	private String cardNo;
+	private Integer status;
+	private String gender;
+	private String email;
+	private String cardPath;
+	private Integer grade;
+
+	public UserInfo getUserInfo() {
+		return userInfo;
+	}
+
+	public void setUserInfo(UserInfo userInfo) {
+		this.userInfo = userInfo;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -42,5 +58,71 @@ public class User extends BasePojo{
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCardNo() {
+		return cardNo;
+	}
+
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCardPath() {
+		return cardPath;
+	}
+
+	public void setCardPath(String cardPath) {
+		this.cardPath = cardPath;
+	}
+
+	public Integer getGrade() {
+		return grade;
+	}
+
+	public void setGrade(Integer grade) {
+		this.grade = grade;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", username='" + username + '\'' +
+				", password='" + password + '\'' +
+				", phone='" + phone + '\'' +
+				", userInfo=" + userInfo +
+				'}';
+	}
 }
